a776be43475c7eb78b5ce3f768ebce2e2ffdebe1
Kingdon Barrett <kingdon.b@nd.edu>
2019-10-05 21:28:19 -0400

    user-defined network

--- pkg/apis/wksprovider/controller/wksctl/machine_actuator.go
+++ pkg/apis/wksprovider/controller/wksctl/machine_actuator.go
@@ -490,7 +490,7 @@ func (a *MachineActuator) getNodePlan(providerSpec *baremetalspecv1.BareMetalClu
 	}
 	plan, err := installer.CreateNodeSetupPlan(os.NodeParams{
 		IsMaster:                 machine.Labels["set"] == "master",
-		MasterIP:                 "172.17.0.2",
+		MasterIP:                 "172.18.0.2",
 		MasterPort:               6443, // TODO: read this dynamically, from somewhere.
 		Token:                    token,
 		DiscoveryTokenCaCertHash: secrets.DiscoveryTokenCaCertHash,
